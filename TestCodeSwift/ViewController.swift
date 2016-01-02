ac917def2713ca749785b150f91cad3dc65e4d89
Vien Vu <lamvienbk@gmail.com>
2016-01-02 09:28:57 +0700

    First Commit

--- TestCodeSwift/ViewController.swift
+++ TestCodeSwift/ViewController.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        let customView = CutomViewDemo(frame:CGRectMake(0, 0, 200, 60))
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        
+        customView.label1.text = "label 1"
+        customView.label2.text = "label 2"
+       
+        self.view.addSubview(customView)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
 
 
 }
